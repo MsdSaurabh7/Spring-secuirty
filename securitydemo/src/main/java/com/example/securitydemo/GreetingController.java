ec701b21e262555c749a9ac4b13d72d900755044
Your Name <saurabhsavekar7@gmail.com>
2025-07-09 23:47:39 +0530

    learn authentication using Inmemory

--- securitydemo/src/main/java/com/example/securitydemo/GreetingController.java
+++ securitydemo/src/main/java/com/example/securitydemo/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.securitydemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,22 @@ public class GreetingController {
 
     @GetMapping("/hello")
     public String sayHello(){
+        return "hello";
+    }
+
+    @GetMapping("/hii")
+    public String sayHii(){
+        return "hello";
+    }
+
+    @GetMapping("/msd")
+    public String sayMsd(){
+        return "hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/csk")
+    public String sayCsk(){
         return "hello";
     }
 }
